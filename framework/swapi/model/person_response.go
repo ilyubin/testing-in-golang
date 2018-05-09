22de6e26966b8c6ec92b571153b292a8edd438fb
igor.lyubin <ilyubin@inbox.ru>
2018-05-09 14:20:59 +0300

    person_response.go

--- framework/swapi/model/person_response.go
+++ framework/swapi/model/person_response.go
@@ -4,8 +4,8 @@ import "time"
 
 type PersonResponse struct {
 	Name      string    `json:"name"`
-	Height    string    `json:"height"`
-	Mass      string    `json:"mass"`
+	Height    int       `json:"height"`
+	Mass      int       `json:"mass"`
 	HairColor string    `json:"hair_color"`
 	SkinColor string    `json:"skin_color"`
 	EyeColor  string    `json:"eye_color"`
